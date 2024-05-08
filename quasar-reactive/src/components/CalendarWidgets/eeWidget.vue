7eb5de0aaa75b925fab839d53b8c9e19c47955b0
AV Patil <abhijit.patil@eagleeye.com>
2024-05-08 22:46:48 +0100

    demo: working

--- quasar-reactive/src/components/CalendarWidgets/eeWidget.vue
+++ quasar-reactive/src/components/CalendarWidgets/eeWidget.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="widget">
+  <div
+    class="widget"
+    id="widget-{{ props.widget.id }}"
+  >
     <q-card class="my-card">
-      <q-card-section class="bg-light-blue text-white">
-        <div class="text-h6"> {{ widget.title }}</div>
-        <div class="text-subtitle2">{{ widget.subtitle }}</div>
+      <q-card-section
+        class="text-white"
+        :style="{ backgroundColor: props.widget.backgroundColor, color: props.widget.textColor }"
+      >
+        <div ref="titleElement"
+             class="text-h6"
+             contenteditable
+             spellcheck="false"
+             @keydown.enter="validate"
+        > {{ props.widget.title }}</div>
+        <div class="text-subtitle2">{{ props.widget.subtitle }}</div>
+        <p>Parent bound v-model is: {{ model }}</p>
       </q-card-section>
 
       <q-separator ></q-separator>
 
       <q-card-actions align="right">
-        <q-btn class="bg-light-green-2">{{ widget.actionText }}</q-btn>
-        <q-btn class="bg-warning">DELETE</q-btn>
+        <q-btn class="bg-light-green-2">{{ props.widget.actionText }}</q-btn>
+        <q-btn class="bg-warning" >DELETE</q-btn>
       </q-card-actions>
     </q-card>
   </div>
 
 </template>
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, ref, watch } from 'vue';
 
-defineProps(['widget']);
+const props = defineProps(['widget']);
+const model = defineModel();
 
+const title = ref(props.widget.title);
+const titleElement = ref(null);
+
+function updateTitle() {
+  model.value = title.value;
+}
+function validate(event : Event) {
+  (event.target as HTMLInputElement).blur();
+  title.value = titleElement.value;
+}
+
+watch(() => props.widget.title, (newVal) => {
+  title.value = newVal;
+});
+
+defineExpose({ titleElement });
 // console.log(props.widget);
 
 </script>
